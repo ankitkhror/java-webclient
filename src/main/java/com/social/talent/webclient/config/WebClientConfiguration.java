a943406c905f44590d7c930e5b6427acc4423d96
Ankit Kumar <ankitkhror@gmail.com>
2022-07-15 12:46:24 +0530

    Web Client

--- src/main/java/com/social/talent/webclient/config/WebClientConfiguration.java
+++ src/main/java/com/social/talent/webclient/config/WebClientConfiguration.java
@@ -3,8 +3,11 @@ package com.social.talent.webclient.config;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
@@ -14,11 +17,15 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebClientConfiguration {
-    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
-    public static final int TIMEOUT = 5000;
+
+    @Value("${api.baseurl}")
+    private String BASE_URL;
+
+    @Value("${api.timeout:5000}")
+    public int TIMEOUT;
 
     @Bean
-    public WebClient webClientWithTimeout() {
+    public WebClient webclient() {
         HttpClient httpClient = HttpClient.create()
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
                 .responseTimeout(Duration.ofMillis(TIMEOUT))
@@ -28,6 +35,7 @@ public class WebClientConfiguration {
 
         return WebClient.builder()
                 .baseUrl(BASE_URL)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
